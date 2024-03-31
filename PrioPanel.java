b48e05bfdfe55ee1199fda98cd29bafe29d92059
Pia Katleya V. Macalanda <130662592+pikavama@users.noreply.github.com>
2024-03-31 12:00:15 +0800

    Update PrioPanel.java
    Components complete. Wala pang application

--- PrioPanel.java
+++ PrioPanel.java
@@ -69,6 +69,111 @@ public class PrioPanel extends JFrame {
         add(tableInfo); 
         add(scrollPane); 
 
+        // RIGHT First Task panel
+        JPanel firstTaskPanel = new JPanel();
+        firstTaskPanel.setBounds(426, 60, 790, 100);
+        //firstTaskPanel.setBackground(Color.ORANGE); // To show panel dimensions only ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧
+        firstTaskPanel.setLayout(null);
+        add(firstTaskPanel);
+
+        // First Task label
+        JLabel firstTaskLabel = new JLabel("What task would you like to start?");
+        firstTaskLabel.setFont(new Font("Canva Sans", Font.PLAIN, 20));
+        firstTaskLabel.setBounds(10, 10, 305, 30);
+        firstTaskPanel.add(firstTaskLabel);
+
+        // First Task textfield
+        CurvedTextField firstTaskTextField = new CurvedTextField();
+        firstTaskTextField.setText("Enter task here");
+        firstTaskTextField.setFont(new Font("Canva Sans", Font.PLAIN, 13));
+        firstTaskTextField.setForeground(Color.decode("#737373"));
+        firstTaskTextField.setBounds(firstTaskLabel.getX() + firstTaskLabel.getWidth() + 10, firstTaskLabel.getY(), 465, 30);
+        firstTaskPanel.add(firstTaskTextField);
+
+        // First Task button
+        RoundedButtonPanel firstTaskButton = new RoundedButtonPanel("Enter");
+        firstTaskButton.setBounds(firstTaskPanel.getWidth()-100, 50, 100, 30);
+        firstTaskPanel.add(firstTaskButton);
+
+        // First Task info label
+        JLabel firstTaskInfoLabel = new JLabel("<html>&#8226; Select task from 'Task Summary' to begin scheduling.</html>");
+        firstTaskInfoLabel.setBounds(10, 50, firstTaskPanel.getWidth()-firstTaskButton.getWidth(), 30);
+        firstTaskInfoLabel.setFont(new Font("Arial", Font.PLAIN, 13));
+        firstTaskInfoLabel.setForeground(Color.decode("#737373"));
+        firstTaskPanel.add(firstTaskInfoLabel);
+
+        // Sched Summary panel
+        JPanel schedSummaryPanel = new JPanel();
+        schedSummaryPanel.setBounds(426, firstTaskPanel.getY() + firstTaskPanel.getHeight() + 10, 790, 100);
+        schedSummaryPanel.setBackground(Color.decode("#D1CBBC"));
+        schedSummaryPanel.setLayout(null);
+        add(schedSummaryPanel);
+
+        // Sched Summary label
+        JLabel  schedSummaryLabel = new JLabel("<html>&#8226; Start your day by accomplishing <br>" +
+                "<br>&#8226; The task is scheduled to start at </html>");
+        schedSummaryLabel.setBounds(20, 10, 250, 80);
+        schedSummaryLabel.setFont(new Font("Arial", Font.PLAIN, 17));
+        schedSummaryPanel.add(schedSummaryLabel);
+
+
+        // Sched Highlight for "Start your day by accomplishing"
+        HighlightPanel schedSummaryHighlight1 = new HighlightPanel();
+        schedSummaryHighlight1.setBounds(schedSummaryLabel.getX() + schedSummaryLabel.getWidth() + 10, 15, 500, 30);
+        schedSummaryPanel.add(schedSummaryHighlight1);
+
+        // Sched Highlight for "The task is scheduled to start at"
+        HighlightPanel schedSummaryHighlight2 = new HighlightPanel();
+        schedSummaryHighlight2.setBounds(schedSummaryLabel.getX() + schedSummaryLabel.getWidth() + 10, 55, 300, 30);
+        schedSummaryPanel.add(schedSummaryHighlight2);
+
+        // Top Priorities Panel
+        HighlightPanel topPrioritiesPanel = new HighlightPanel();
+        topPrioritiesPanel.setBounds(426, schedSummaryPanel.getY() + schedSummaryPanel.getHeight() + 35, 790, 33);
+        add(topPrioritiesPanel);
+
+        // Top Priorities label
+        JLabel topPrioritiesLabel = new JLabel("T O P  P R I O R I T I E S");
+        topPrioritiesLabel.setForeground(Color.WHITE);
+        topPrioritiesLabel.setFont(new Font("Canva Sans", Font.BOLD, 14));
+        topPrioritiesLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        topPrioritiesLabel.setBounds(10, 20, 280, 20);
+        topPrioritiesPanel.add(topPrioritiesLabel);
+
+        // Top Priorities Table / TPT
+        DefaultTableModel topPrioritiesTableModel = new DefaultTableModel();
+        topPrioritiesTableModel.addColumn("Date");
+        topPrioritiesTableModel.addColumn("Time");
+        topPrioritiesTableModel.addColumn("Task");
+
+        JTable topPrioritiesTable = new JTable(topPrioritiesTableModel);
+        topPrioritiesTable.setBounds(426, topPrioritiesPanel.getY() + topPrioritiesPanel.getHeight() + 20, 790, 250);
+
+        // TPT: Set column header attributes
+        JTableHeader topPrioritiesHeader = topPrioritiesTable.getTableHeader();
+        topPrioritiesHeader.setBackground(new Color(190, 182, 168));
+        topPrioritiesHeader.setForeground(Color.WHITE);
+        topPrioritiesHeader.setReorderingAllowed(false);
+        topPrioritiesHeader.setResizingAllowed(false);
+
+        // TPT: Set preferred widths for columns
+        TableColumnModel topPrioritiesColumnModel = topPrioritiesTable.getColumnModel();
+        TableColumn topPrioritiesColumnDate = topPrioritiesColumnModel.getColumn(0);
+        TableColumn topPrioritiesColumnTime = topPrioritiesColumnModel.getColumn(1);
+        TableColumn topPrioritiesColumnName = topPrioritiesColumnModel.getColumn(2);
+
+        topPrioritiesColumnDate.setPreferredWidth(30);
+        topPrioritiesColumnTime.setPreferredWidth(30);
+        topPrioritiesColumnName.setPreferredWidth(180);
+
+        topPrioritiesTable.getTableHeader().setPreferredSize(new Dimension(topPrioritiesTable.getTableHeader().getWidth(), 30));
+        topPrioritiesTable.setRowHeight(25);
+
+        // TPT: Add the Top Priorities table to the frame
+        JScrollPane topPrioritiesScrollPane = new JScrollPane(topPrioritiesTable);
+        topPrioritiesScrollPane.setBounds(426, topPrioritiesPanel.getY() + topPrioritiesPanel.getHeight() + 20, 790, 250);
+        add(topPrioritiesScrollPane);
+
         setVisible(true);
     }
 
