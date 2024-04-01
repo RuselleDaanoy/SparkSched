715d220f55aa61fbaad4bcc6bb17558c380d8ad6
Pia Katleya V. Macalanda <130662592+pikavama@users.noreply.github.com>
2024-04-01 18:49:46 +0800

    Panel 3

---
+++ SchedRoutineSummary.java
@@ -0,0 +1,91 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+public class SchedRoutineSummary extends JFrame {
+
+    private DefaultTableModel tableModel;
+    private JTable dataTable;
+    private JLabel infoLabel;
+
+    public SchedRoutineSummary() {
+
+        setTitle("Spark Sched");
+        setSize(460, 720);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        ImageIcon logo = new ImageIcon("src/SparkSchedLogo.png");
+        setIconImage(logo.getImage());
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        add(mainPanel);
+
+        HighlightPanel taskSummary = new HighlightPanel();
+        taskSummary.setPreferredSize(new Dimension(340, 33));
+
+        JLabel label = new JLabel("R O U T I N E  S U M M A R Y");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("Canva Sans", Font.BOLD, 14));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        taskSummary.add(label);
+        mainPanel.add(taskSummary, BorderLayout.NORTH);
+
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("Date");
+        tableModel.addColumn("Time");
+        tableModel.addColumn("Task");
+
+        dataTable = new JTable(tableModel);
+        JTableHeader header = dataTable.getTableHeader();
+        header.setBackground(new Color(190, 182, 168));
+        header.setForeground(Color.WHITE);
+        dataTable.getTableHeader().setReorderingAllowed(false);
+        dataTable.getTableHeader().setResizingAllowed(false);
+
+        TableColumnModel columnModel = dataTable.getColumnModel();
+        columnModel.getColumn(0).setPreferredWidth(30);
+        columnModel.getColumn(1).setPreferredWidth(30);
+        columnModel.getColumn(2).setPreferredWidth(190);
+
+        JScrollPane scrollPane = new JScrollPane(dataTable);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+
+        JPanel taskSummaryInfo = new JPanel();
+        taskSummaryInfo.setPreferredSize(new Dimension(340, 50));
+        //taskSummaryInfo.setBackground(Color.PINK);
+        taskSummaryInfo.setLayout(null); // Set layout to null
+        mainPanel.add(taskSummaryInfo, BorderLayout.SOUTH);
+
+        infoLabel = new JLabel("<html>&#8226; This table utilizes <b>Depth-First Search (DFS)</b>, " +
+                "the task routine is efficiently sorted starting based on your chosen task, " +
+                "ensuring tasks are sequenced in a manner that respects their dependencies and maximizes efficiency.<br></html>");
+        infoLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        infoLabel.setForeground(Color.decode("#737373"));
+        infoLabel.setPreferredSize(new Dimension(466, 80));
+        taskSummaryInfo.add(infoLabel);
+
+        // Add a ComponentListener to the frame to listen for resize eventszzzz
+        addComponentListener(new ComponentAdapter() {
+
+            @Override
+            public void componentResized(ComponentEvent e) {
+                // Resize the label dynamically based on the new size of the frame
+                Dimension size = taskSummaryInfo.getSize();
+                infoLabel.setBounds(0, 0, size.width, size.height);
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(SchedRoutineSummary::new);
+    }
+}
